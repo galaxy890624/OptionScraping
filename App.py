6ab83940b2384e14d8b43ca955af4b472530d7b8
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-12-16 09:51:10 +0800

    Update ..\templates\Index.html
    修復 按下"刷新資料"按鈕後 無法爬取表格資料的bug

--- App.py
+++ App.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-from Delta import fetch_options_data, get_spot_price_taifex, calculate_delta, current_time, datetime, spot_price
+from flask_cors import CORS
+from Delta import fetch_options_data, get_spot_price_taifex, calculate_delta, current_time, datetime, spot_price, calculate_days_to_maturity
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/') # http://127.0.0.1:5000
 def home():
     current_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")  # 格式化當前時間
-    return render_template('Index.html', current_time=current_time, spot_price = spot_price)
+    return render_template('Index.html', current_time=current_time)
 
 @app.route('/api/options', methods=['GET'])
 def get_options():
@@ -20,8 +22,9 @@ def get_options():
         # 計算 Delta
         risk_free_rate = 0.02  # 無風險利率
         volatility = 0.25  # 波動率
-        time_to_maturity_days = 7  # 到期日剩餘天數
-        time_to_maturity = time_to_maturity_days / 365  # 轉換為年
+        expiration_date = datetime(2024, 12, 18, 13, 30) # 手動修改
+        time_to_maturity_days = calculate_days_to_maturity(expiration_date)
+        time_to_maturity = time_to_maturity_days / 365
 
         deltas = []
         for _, row in df.iterrows():
@@ -35,6 +38,14 @@ def get_options():
         # 返回 JSON 格式
         # 包括 spot_price 一起返回
         return jsonify({"spot_price": spot_price, "options": df.to_dict(orient="records")})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/api/spot_price', methods=['GET'])
+def get_spot_price():
+    try:
+        spot_price = get_spot_price_taifex()
+        return jsonify({"spot_price": spot_price})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
